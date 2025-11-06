10ac4a8b9dbb07ebcbbdfb9a41ea21d91e69cf54
haytham5 <78439856+haytham5@users.noreply.github.com>
2025-11-05 19:21:40 -0500

    Consolidating Controllers

--- test/controllers/HomeControllerTest.java
+++ test/controllers/HomeControllerTest.java
@@ -1,9 +1,11 @@
 package controllers;
 
+import Services.Client;
 import models.Article;
 import models.QueryResult;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
 import play.libs.ws.*;
 import play.mvc.Http;
@@ -15,6 +17,9 @@ import java.util.*;
 import java.util.concurrent.*;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.when;
 import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.*;
 
@@ -260,5 +265,40 @@ public class HomeControllerTest {
         controller.setCache(m);
         assertSame(m, controller.getCache());
         assertTrue(HomeController.getMaxArticlesVisible() > 0);
+    }
+
+
+    @Test
+    public void testProfile_NoArticles() throws ExecutionException, InterruptedException, TimeoutException {
+        try(MockedConstruction<Client> mockedClient = mockConstruction(Client.class, (mock, context) -> {
+            when(mock.clientRequest(anyString())).thenReturn(CompletableFuture.completedFuture(new ArrayList<> ()));
+        })) {
+            CompletionStage<Result> stage = controller.profile("TestSourceName", "TestID");
+            Result result = stage.toCompletableFuture().get(2, TimeUnit.SECONDS);
+
+            assertEquals(OK, result.status());
+            String body = contentAsString(result);
+            assertTrue(body.contains("No Articles Found for this source"));
+        }
+    }
+
+    @Test
+    public void testProfile() throws ExecutionException, InterruptedException, TimeoutException {
+        Article testArticle = new Article("Title One", "url1", "Source1", "sourceUrl1", "2025-01-01, 12:00:00", 5, 5, "Description One");
+
+        List<Article> articles = List.of(testArticle);
+
+        try (MockedConstruction<Client> mockedClient = mockConstruction(Client.class, (mock, context) -> {
+            when(mock.clientRequest(anyString())).thenReturn(CompletableFuture.completedFuture(articles));
+        })) {
+            CompletionStage<Result> stage = controller.profile("BBC", null);
+            Result result = stage.toCompletableFuture().get(2, TimeUnit.SECONDS);
+
+            assertEquals(OK, result.status());
+            String body = contentAsString(result);
+
+            assertTrue(body.contains("Listing Articles from BBC"));
+            assertTrue(body.contains("sourceUrl1"));
+        }
     }
 }
