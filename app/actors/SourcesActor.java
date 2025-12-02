8475a3f3811f915247501fdc9dee93526a41dcf8
ihasnaou <ia.hasnaou@gmail.com>
2025-12-02 16:50:14 -0500

    More javadoc for sources

--- app/actors/SourcesActor.java
+++ app/actors/SourcesActor.java
@@ -12,9 +12,16 @@ import java.util.stream.Collectors;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Implements the actor returning the list of sources
+ * @author ihasnaou
+ */
 public final class SourcesActor {
     private SourcesActor() {}
 
+    /**
+     * Message containing list of sources
+     */
     public static class Sources {
         final Set<Source> sources;
 
@@ -23,6 +30,9 @@ public final class SourcesActor {
         }
     }
 
+    /**
+     * Message asking for list of sources
+     */
     public static final class GetSources {
         final Set<String> sourceIds;
         final ActorRef<Sources> replyTo;
